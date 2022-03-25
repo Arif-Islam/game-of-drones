49059da59914b273c1905db16177c64a481f2bf2
Arif <iariful950@gmail.com>
2022-03-26 01:07:35 +0600

    site name and subtitle added!

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Subtitle from './components/Subtitle/Subtitle';
+import Title from './components/Ttitle/Title';
 
 function App() {
   return (
     <div>
-      
+      <Title></Title>
+      <Subtitle></Subtitle>
     </div>
   );
 }
